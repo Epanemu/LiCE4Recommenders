1798639328162e59953bfebf9a0fe975408bb655
Jiri Nemecek <contact@nemecekjiri.cz>
2025-04-12 21:53:11 +0200

    Fix SPN naming and data loading

--- 2_train_models.py
+++ 2_train_models.py
@@ -1,12 +1,14 @@
-from ease_model import EASE
-import pickle
-import pandas as pd
-from LiCE.spn.SPN import SPN
-from LiCE.data.DataHandler import DataHandler
-from scipy.sparse import hstack, csc_matrix
 import os
+import pickle
 import sys
+
 import numpy as np
+import pandas as pd
+from scipy.sparse import csc_matrix, hstack
+
+from ease_model import EASE
+from LiCE.data.DataHandler import DataHandler
+from LiCE.spn.SPN import SPN
 
 data_name = sys.argv[1]
 fold = int(sys.argv[2])
@@ -14,16 +16,18 @@ group_f = sys.argv[3]
 rating_used = sys.argv[4] == "rating"
 
 help_folder = "data_splits"
-max_samples_for_spn = 50_000
+max_samples_for_spn = 5_000_000
 min_instances_spn = 5000
 
 os.makedirs(f"{help_folder}/{data_name}/{fold}/models", exist_ok=True)
 
-df = pd.read_csv(f"{help_folder}/{data_name}/df.csv")
+df = pd.read_csv(f"{help_folder}/{data_name}/df.csv").astype(
+    {"user_id": str, "item_id": str, "rating": int}
+)
 with open(f"{help_folder}/{data_name}/{fold}/groups.pickle", "rb") as f:
     (groups, categ_order) = pickle.load(f)
 
-max_rating = df['rating'].max() if rating_used else 1
+max_rating = df["rating"].max() if rating_used else 1
 
 with open(f"{help_folder}/{data_name}/{fold}/train.pickle", "rb") as f:
     train_users = pickle.load(f)
@@ -31,43 +35,71 @@ with open(f"{help_folder}/{data_name}/{fold}/train.pickle", "rb") as f:
 df = df[df["user_id"].isin(train_users)]
 
 ease = EASE()
-ease.fit(df, implicit=not rating_used) # implicit = True means that rating is disregarded (always 1)
+ease.fit(
+    df, implicit=not rating_used
+)  # implicit = True means that rating is disregarded (always 1)
 
-with open(f"{help_folder}/{data_name}/{fold}/models/ease_{'rating' if rating_used else 'binary'}.pickle", "wb") as f:
+with open(
+    f"{help_folder}/{data_name}/{fold}/models/ease_{'rating' if rating_used else 'binary'}.pickle",
+    "wb",
+) as f:
     pickle.dump(ease, f)
 
 # -- train SPN
 groups = [ease.item_enc.transform(items) for items in groups]
-bounds = {c:(0, 1) for c in categ_order}
+bounds = {c: (0, 1) for c in categ_order}
 discrete = [] if rating_used else categ_order
 cat_map = {}
+feature_names = categ_order
 if group_f == "sum":
-    spn_train_data = [csc_matrix(ease.X[:,g].sum(axis=1)) for g in groups]
-    bounds = {c:(0, spn_train_data[i].max()) for i, c in enumerate(categ_order)}
+    spn_train_data = [csc_matrix(ease.X[:, g].sum(axis=1)) for g in groups]
+    bounds = {c: (0, spn_train_data[i].max()) for i, c in enumerate(categ_order)}
 elif group_f == "mean":
-    spn_train_data = [csc_matrix(ease.X[:,g].mean(axis=1)) for g in groups]
+    spn_train_data = [csc_matrix(ease.X[:, g].mean(axis=1)) for g in groups]
     discrete = []
 elif group_f == "disjunction":
-    spn_train_data = [csc_matrix(ease.X[:,g].sum(axis=1) > 0).astype(int) for g in groups]
+    spn_train_data = [
+        csc_matrix(ease.X[:, g].sum(axis=1) > 0).astype(int) for g in groups
+    ]
     cat_map = {c: [0, 1] for c in categ_order}
     discrete = categ_order
 elif group_f == "none":
     spn_train_data = [(ease.X * max_rating).astype(int)]
-    bounds = {c:(0, max_rating) for c in categ_order}
+    bounds = {c: (0, max_rating) for c in categ_order}
+    feature_names = list(ease.item_enc.classes_)
     if not rating_used:
         cat_map = {c: [0, 1] for c in categ_order}
 
 spn_train_data = hstack(spn_train_data).toarray()
-data_handler = DataHandler(spn_train_data, categ_map=cat_map, bounds_map=bounds, discrete=discrete, feature_names=categ_order)
+data_handler = DataHandler(
+    spn_train_data,
+    categ_map=cat_map,
+    bounds_map=bounds,
+    discrete=discrete,
+    feature_names=feature_names,
+)
 
 if spn_train_data.shape[0] > max_samples_for_spn:
     np.random.seed(fold)
-    subset = np.random.choice(spn_train_data.shape[0], max_samples_for_spn, replace=False)
-    spn = SPN(spn_train_data[subset, :], data_handler, learn_mspn_kwargs={"min_instances_slice": min_instances_spn})
+    subset = np.random.choice(
+        spn_train_data.shape[0], max_samples_for_spn, replace=False
+    )
+    spn = SPN(
+        spn_train_data[subset, :],
+        data_handler,
+        learn_mspn_kwargs={"min_instances_slice": min_instances_spn},
+    )
     lls = spn.compute_ll(spn_train_data[subset, :])
 else:
-    spn = SPN(spn_train_data, data_handler, learn_mspn_kwargs={"min_instances_slice": min_instances_spn})
+    spn = SPN(
+        spn_train_data,
+        data_handler,
+        learn_mspn_kwargs={"min_instances_slice": min_instances_spn},
+    )
     lls = spn.compute_ll(spn_train_data)
 
-with open(f"{help_folder}/{data_name}/{fold}/models/spn_{group_f}.pickle", "wb") as f:
+with open(
+    f"{help_folder}/{data_name}/{fold}/models/spn_{group_f}_{'rating' if rating_used else 'binary'}.pickle",
+    "wb",
+) as f:
     pickle.dump((spn, lls), f)
--- 3_get_CEs.py
+++ 3_get_CEs.py
@@ -1,10 +1,12 @@
-import pickle
 import os
+import pickle
 import sys
-from LiCE.lice.LiCE import LiCE
-import numpy as np
 from collections import defaultdict
+
+import numpy as np
 import pandas as pd
+
+from LiCE.lice.LiCE import LiCE
 
 data_name = sys.argv[1]
 fold = int(sys.argv[2])
@@ -13,7 +15,7 @@ rating_used = sys.argv[4] == "rating"
 thresholding = sys.argv[5] == "median"
 no_spn = sys.argv[5] == "no_spn"
 if not thresholding and not no_spn:
-    opt_coeff = float(sys.argv[5]) # 0.05
+    opt_coeff = float(sys.argv[5])  # 0.05
 nth = sys.argv[6] == "nth"
 
 time_limit = 600
@@ -22,13 +24,18 @@ help_folder = "data_splits"
 
 os.makedirs(f"{help_folder}/{data_name}/{fold}/CEs", exist_ok=True)
 
-df = pd.read_csv(f"{help_folder}/{data_name}/df.csv")
+df = pd.read_csv(f"{help_folder}/{data_name}/df.csv").astype(
+    {"user_id": str, "item_id": str, "rating": int}
+)
 with open(f"{help_folder}/{data_name}/{fold}/groups.pickle", "rb") as f:
     (groups, categ_order) = pickle.load(f)
 
-max_rating = df['rating'].max() if rating_used else 1
+max_rating = df["rating"].max() if rating_used else 1
 
-with open(f"{help_folder}/{data_name}/{fold}/models/ease_{'rating' if rating_used else 'binary'}.pickle", "rb") as f:
+with open(
+    f"{help_folder}/{data_name}/{fold}/models/ease_{'rating' if rating_used else 'binary'}.pickle",
+    "rb",
+) as f:
     ease = pickle.load(f)
 
 with open(f"{help_folder}/{data_name}/{fold}/test.pickle", "rb") as f:
@@ -36,7 +43,10 @@ with open(f"{help_folder}/{data_name}/{fold}/test.pickle", "rb") as f:
 
 df = df[df["user_id"].isin(test_users)]
 
-with open(f"{help_folder}/{data_name}/{fold}/models/spn_{group_f}.pickle", "rb") as f:
+with open(
+    f"{help_folder}/{data_name}/{fold}/models/spn_{group_f}_{'rating' if rating_used else 'binary'}.pickle",
+    "rb",
+) as f:
     spn, lls = pickle.load(f)
     if thresholding:
         median_ll = np.median(lls)
@@ -54,18 +64,49 @@ for user_id, rows in df.groupby(["user_id"]):
     for j, expl_i in enumerate(ease.item_enc.transform(preds.iloc[:3]["item_id"])):
         if nth:
             if thresholding:
-                res = lice.generate_counterfactual(factual, expl_i, nth=topk+1, time_limit=time_limit, ll_threshold=median_ll)
+                res = lice.generate_counterfactual(
+                    factual,
+                    expl_i,
+                    nth=topk + 1,
+                    time_limit=time_limit,
+                    ll_threshold=median_ll,
+                )
             elif not no_spn:
-                res = lice.generate_counterfactual(factual, expl_i, nth=topk+1, time_limit=time_limit, ll_opt_coefficient=opt_coeff)
+                res = lice.generate_counterfactual(
+                    factual,
+                    expl_i,
+                    nth=topk + 1,
+                    time_limit=time_limit,
+                    ll_opt_coefficient=opt_coeff,
+                )
             else:
-                res = lice.generate_counterfactual(factual, expl_i, nth=topk+1, time_limit=time_limit)
+                res = lice.generate_counterfactual(
+                    factual, expl_i, nth=topk + 1, time_limit=time_limit
+                )
         else:
             if thresholding:
-                res = lice.generate_counterfactual(factual, expl_i, score=preds.iloc[-1]["score"], time_limit=time_limit, ll_threshold=median_ll)
+                res = lice.generate_counterfactual(
+                    factual,
+                    expl_i,
+                    score=preds.iloc[-1]["score"],
+                    time_limit=time_limit,
+                    ll_threshold=median_ll,
+                )
             elif not no_spn:
-                res = lice.generate_counterfactual(factual, expl_i, score=preds.iloc[-1]["score"], time_limit=time_limit, ll_opt_coefficient=opt_coeff)
+                res = lice.generate_counterfactual(
+                    factual,
+                    expl_i,
+                    score=preds.iloc[-1]["score"],
+                    time_limit=time_limit,
+                    ll_opt_coefficient=opt_coeff,
+                )
             else:
-                res = lice.generate_counterfactual(factual, expl_i, score=preds.iloc[-1]["score"], time_limit=time_limit)
+                res = lice.generate_counterfactual(
+                    factual,
+                    expl_i,
+                    score=preds.iloc[-1]["score"],
+                    time_limit=time_limit,
+                )
 
         scores = res[0] @ ease.B
         true_position = np.sum(scores > scores[expl_i])
@@ -89,4 +130,12 @@ for user_id, rows in df.groupby(["user_id"]):
             spn_f = factual * max_rating
         stats["couterfactual_ll"] = spn.compute_ll(spn_cf)
         stats["factual_ll"] = spn.compute_ll(spn_f)
+        stats["distance"] = np.abs(factual - res[0])
+        stats["sparsity"] = np.sum(np.isclose(factual, res[0], atol=0.0001))
         allstats[user_id][expl_i](stats)
+
+with open(
+    f"{help_folder}/{data_name}/{fold}/CEs/stats_{sys.argv[5]}_{sys.argv[6]}.pickle",
+    "wb",
+) as f:
+    pickle.dump(allstats, f)
